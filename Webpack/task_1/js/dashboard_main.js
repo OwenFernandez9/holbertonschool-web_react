fceaa3b64197ae10e3ba12e229218767b68cc4f5
OwenFernandez9 <9357@holbertonstudents.com>
2025-09-21 22:58:45 -0300

    task1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="startBtn">Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let clicks = 0;
+function updateCounter() {
+  clicks += 1;
+  $('#count').text(`${clicks} clicks on the button`);
+}
+
+const onClickDebounced = debounce(updateCounter, 500);
+
+$(document).on('click', '#startBtn', onClickDebounced);
