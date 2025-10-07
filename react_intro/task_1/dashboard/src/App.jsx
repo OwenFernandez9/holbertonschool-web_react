ab37308c5baff05f7e46ed5d42c9ee6308aaf28f
OwenFernandez9 <9357@holbertonstudents.com>
2025-10-07 15:07:33 -0300

    task 1

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import './App.css';
+import logo from './assets/holberton-logo.jpg';
 import Notifications from './Notifications';
 import { getCurrentYear, getFooterCopy } from './utils';
 
 function App() {
   return (
     <>
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" className="App-logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
       <div className="root-notifications">
         <Notifications />
       </div>
