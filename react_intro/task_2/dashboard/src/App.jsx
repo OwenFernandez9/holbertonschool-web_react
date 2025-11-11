c38a70fdd59e18bef1fa28ee25a6e5797ea6d876
OwenFernandez9 <9357@holbertonstudents.com>
2025-11-11 12:54:00 -0300

    task2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -6,36 +6,38 @@ import { getCurrentYear, getFooterCopy } from './utils';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} alt="holberton logo" className="App-logo" />
-        <h1>School dashboard</h1>
-      </div>
+      <>
+        
+        <div className="root-notifications">
+          <Notifications />
+        </div>
 
-      <div className="root-notifications">
-        <Notifications />
-      </div>
-      
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+        <div className="App-header">
+          <img src={logo} alt="holberton logo" className="App-logo" />
+          <h1>School dashboard</h1>
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
 
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+          <label htmlFor="email">Email:</label>
+          <input placeholder="email" type="email" id="email" />
 
-        <button>OK</button>
-      </div>
+          <label htmlFor="password">Password</label>
+          <input placeholder="password" type="password" id="password" />
+
+          <button>OK</button>
+        </div>
 
 
-      <div className="App-footer">
-        <p>
-          Copyright {getFooterCopy(false)} - {getCurrentYear()}
-        </p>
-      </div>
-    </div>
-  );
-}
+        <div className="App-footer">
+          <p>
+            Copyright {getFooterCopy(false)} - {getCurrentYear()}
+          </p>
+        </div>
+      </>
+    )
+  }
 
 export default App;
